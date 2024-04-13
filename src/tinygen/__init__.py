7c9e01e732b674d0a3b891186fca9b4528b238e8
Ellen Agarwal <57874654+bagel897@users.noreply.github.com>
2024-04-13 16:46:50 +0000

    Feed files to openai

--- src/tinygen/__init__.py
+++ src/tinygen/__init__.py
@@ -1,9 +1,17 @@
+import os
+from pathlib import Path
+from tarfile import SUPPORTED_TYPES
 from fastapi import FastAPI
+from openai.types.beta.threads import Text
 from pydantic import BaseModel
 from git import Repo
+from openai import OpenAI, AssistantEventHandler
+from loguru import logger
 import tempfile
 
 app = FastAPI()
+
+MODEL = "gpt-3.5-turbo"
 
 
 class InputData(BaseModel):
@@ -11,16 +19,116 @@ class InputData(BaseModel):
     prompt: str
 
 
+def get_diff(repo: Repo):
+    diff = repo.index.diff(None)
+    return diff
+
+
+TOOLS = [
+    {
+        "type": "function",
+        "name": "read_file",
+        "description": "Reads a file and returns its contents",
+        "parameters": [
+            {
+                "type": "object",
+                "properties": {
+                    "file": {"type": "string"},
+                },
+                "required": ["file"],
+            }
+        ],
+    }
+]
+
+SUPPORTED_TYPES = [".c", ".cpp", ".py", ".sh", ".md", ".html", ".txt"]
+
+
+def get_files(tree):
+    for entry in tree:
+        if entry.type == "tree":
+            yield from get_files(entry)
+        else:
+            if entry.name.endswith(tuple(SUPPORTED_TYPES)):
+
+                yield entry.path
+
+
+def upload_files(client, files, working_dir: Path):
+    for file in files:
+        name = file
+        full_path = (working_dir / file).resolve()
+        content = full_path.read_bytes()
+        if len(content) == 0:
+            continue
+        logger.debug(name, full_path, content)
+        yield client.files.create(
+            file=(name, content),
+            purpose="assistants",
+        )
+
+
+def get_suggestions(client: OpenAI, repo: Repo, prompt: str, working_dir: Path):
+    files = list(get_files(repo.head.commit.tree))
+    os.system(f"ls -l {working_dir}")
+
+    openai_files = list(upload_files(client, files, working_dir))
+
+    class EventHandler(AssistantEventHandler):
+        def on_message_done(self, message):
+            logger.debug(message)
+
+        def on_text_done(self, text: Text) -> None:
+            logger.debug(text)
+            for annotation in text.annotations:
+                logger.debug(annotation)
+                if annotation.type == "file_path":
+                    file_data = client.files.content(annotation.file_path.file_id)
+                    working_dir.joinpath(file_data.filename).write_text(
+                        file_data.read()
+                    )
+            return super().on_text_done(text)
+
+        # def on_event(self, event) -> None:
+        #     logger.debug(event)
+        #     return super().on_event(event)
+
+        # @override
+        # def on_text_delta(self, delta, snapshot):
+        #     logger.debug(delta.value)
+
+    assistant = client.beta.assistants.create(
+        name="tinygen",
+        model=MODEL,
+        description="Edit the files to apply the requested change",
+        tools=[{"type": "code_interpreter"}],
+        file_ids=[file.id for file in openai_files],
+    )
+    thread = client.beta.threads.create(
+        messages=[
+            {"role": "user", "content": "prompt:\n" + prompt},
+        ],
+    )
+    with client.beta.threads.runs.stream(
+        thread_id=thread.id, assistant_id=assistant.id, event_handler=EventHandler()
+    ) as stream:
+        stream.until_done()
+
+
 @app.post("/change")
 def change_repo(data: InputData):
+    client = OpenAI()
+
     with tempfile.TemporaryDirectory() as tempdir:
         # Step 1: Fetch/clone repo
         repo = Repo.clone_from(data.repoUrl, tempdir)
+
         # Step 2: Edit repo
+        get_suggestions(client, repo, data.prompt, Path(tempdir))
         # Step 3: Calculate diff
-        result = repo.index.diff(None)
+        result = get_diff(repo)
         # TODO actually get unified diff from diff object
-        print(result)
+        logger.debug(result)
         # Step 4: Reflection via GPT
 
     # Add your code here to process the repoUrl and prompt
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,26 @@ from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
-DEFAULT_REPO = "https://github.com/bagel897/bagel897"
+DEFAULT_REPO = "https://github.com/jayhack/llm.sh"
+DEFAULT_PROMPT = r"""# The program doesn't output anything in windows 10
+
+(base) C:\Users\off99\Documents\Code\>llm list files in current dir; windows
+/ Querying GPT-3200
+───────┬────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+       │ File: temp.sh
+───────┼────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+   1   │
+   2   │ dir
+   3   │ ```
+───────┴────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+>> Do you want to run this program? [Y/n] y
+
+Running...
+
+
+(base) C:\Users\off99\Documents\Code\>
+Notice that there is no output. Is this supposed to work on Windows also?
+Also it might be great if the script detects which OS or shell I'm using and try to use the appropriate command e.g. dir instead of ls because I don't want to be adding windows after every prompt."""
 
 
 def get_results(
@@ -15,7 +34,7 @@ def get_results(
 
 
 def test_basic():
-    result = get_results(client, "Add an expanation of how I went to Mt Everest")
+    result = get_results(client, DEFAULT_PROMPT)
     assert result.status_code == 200
     print(result.json())
     assert False
