5063c1125d5f46914a4cfe0fea0fd4f8ee7dba64
Ellen Agarwal <57874654+bagel897@users.noreply.github.com>
2024-04-14 21:19:14 +0000

    Refactor assistant and File

--- src/tinygen/app.py
+++ src/tinygen/app.py
@@ -2,96 +2,30 @@ import json
 from pathlib import Path
 from git import Repo
 from loguru import logger
-from openai import AssistantEventHandler, OpenAI
+from openai import OpenAI
 from tinygen.consts import MODEL
 from tinygen.git_utils import get_files
-from tinygen.openai_utils import upload_files
+from tinygen.openai_utils import Assistant, File
 
 
 def get_suggestions(client: OpenAI, repo: Repo, prompt: str, working_dir: Path):
-    files = list(get_files(repo.head.commit.tree))
-    openai_files = list(upload_files(client, files, working_dir))
-    TOOLS = {
-        "type": "function",
-        "function": {
-            "name": "write_file",
-            "description": "Writes a new version of a file in the repository",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "path": {
-                        "type": "string",
-                        "enum": files,
-                        "description": "The original filename relative to the root of the repository",
-                    },
-                    "content": {
-                        "type": "string",
-                        "description": "The new version of the file",
-                    },
-                },
-                "required": ["path", "content"],
-            },
-        },
-    }
+    files = [
+        File(working_dir / path, working_dir, client)
+        for path in get_files(repo.head.commit.tree)
+    ]
 
-    class EventHandler(AssistantEventHandler):
-        # def on_message_done(self, message):
-        #     logger.debug(message)
-
-        def on_text_done(self, text) -> None:
-            logger.debug(text.value)
-            for annotation in text.annotations:
-                logger.trace(annotation)
-                if annotation.type == "file_path":
-                    file_data = client.files.content(annotation.file_path.file_id)
-                    logger.debug(file_data)
-                    self.write_file(file_data.filename, file_data.read())
-
-            return super().on_text_done(text)
-
-        def write_file(self, path, content):
-            full_path = working_dir.joinpath(path)
-            if full_path.exists():
-                full_path.write_text(content)
-            return path
-
-        def on_tool_call_done(
-            self,
-            tool_call,
-        ) -> None:
-            if tool_call.type == "function":
-                if tool_call.function.name == "write_file":
-                    logger.trace(tool_call.function.arguments)
-                    arguments = json.loads(tool_call.function.arguments)
-                    logger.trace(arguments)
-                    self.write_file(
-                        arguments["path"],
-                        arguments["content"],
-                    )
-            return super().on_tool_call_done(tool_call)
-
-    assistant = client.beta.assistants.create(
+    assistant = Assistant(
         name="tinygen",
-        model=MODEL,
-        instructions=f"You are an assistant who fixes the problem given by the user. You do this by modifiying the following files: {files}. You only make the necessary changes to fix the user's problem and preseve the functionality of the program. You may not ask questions, just make the change.",
-        tools=[{"type": "retrieval"}, TOOLS],
-        file_ids=[file.id for file in openai_files],
+        prompt=f"You are an assistant who fixes the problem given by the user. You do this by modifiying the following files: {(file.name for file in files if file.name is not None)}. You only make the necessary changes to fix the user's problem and preseve the functionality of the program. You may not ask questions, just make the change.",
+        client=client,
+        files=files,
     )
     try:
-
-        thread = client.beta.threads.create(
-            messages=[
-                {"role": "user", "content": prompt},
-            ],
-        )
-        with client.beta.threads.runs.stream(
-            thread_id=thread.id, assistant_id=assistant.id, event_handler=EventHandler()
-        ) as stream:
-            stream.until_done()
+        assistant.run_thread(prompt, working_dir)
     finally:
-        client.beta.assistants.delete(assistant.id)
-        for file in openai_files:
-            client.files.delete(file.id)
+        assistant.close()
+        for file in files:
+            file.close()
 
 
 def is_change_good(change: str, prompt: str, client: OpenAI) -> bool:
--- src/tinygen/openai_utils.py
+++ src/tinygen/openai_utils.py
@@ -1,18 +1,129 @@
 """OpenAI helpers"""
 
+import json
 from pathlib import Path
 from loguru import logger
+import openai
+from openai import OpenAI
+
+from tinygen.consts import MODEL
 
 
-def upload_files(client, files, working_dir: Path):
-    for file in files:
-        name = file
-        full_path = (working_dir / file).resolve()
-        content = full_path.read_bytes()
+class File:
+    path: Path
+    name: str
+    openai = None
+    client: OpenAI
+
+    def __init__(self, path: Path, working_dir: Path, client: OpenAI) -> None:
+        self.path = path
+        self.name = str(path.relative_to(working_dir))
+        content = path.read_bytes()
+        self.client = client
         if len(content) == 0:
-            continue
-        logger.trace(f"Uploaded {name}")
-        yield client.files.create(
-            file=(name, content),
-            purpose="assistants",
+            self.openai = None
+        else:
+            logger.trace(f"Uploaded {self.name}")
+            self.client = client
+            self.openai = client.files.create(
+                file=(self.name, content),
+                purpose="assistants",
+            )
+
+    def close(self):
+        if self.openai is not None:
+            self.client.files.delete(self.openai.id)
+
+
+class Assistant:
+    client: OpenAI
+
+    def __init__(
+        self, name: str, prompt: str, client: OpenAI, files: list[File]
+    ) -> None:
+        self.client = client
+        TOOLS = {
+            "type": "function",
+            "function": {
+                "name": "write_file",
+                "description": "Writes a new version of a file in the repository",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "path": {
+                            "type": "string",
+                            "enum": [
+                                file.name for file in files if file.openai is not None
+                            ],
+                            "description": "The original filename relative to the root of the repository",
+                        },
+                        "content": {
+                            "type": "string",
+                            "description": "The new version of the file",
+                        },
+                    },
+                    "required": ["path", "content"],
+                },
+            },
+        }
+        self.assistant = self.client.beta.assistants.create(
+            name=name,
+            model=MODEL,
+            instructions=prompt,
+            tools=[{"type": "retrieval"}, TOOLS],
+            file_ids=[file.openai.id for file in files if file.openai is not None],
         )
+
+    def run_thread(self, prompt: str, working_dir: Path):
+        class EventHandler(openai.AssistantEventHandler):
+            # def on_message_done(self, message):
+            #     logger.debug(message)
+
+            def on_text_done(self, text) -> None:
+                logger.debug(text.value)
+                for annotation in text.annotations:
+                    logger.trace(annotation)
+                    if annotation.type == "file_path":
+                        file_data = self.client.files.content(
+                            annotation.file_path.file_id
+                        )
+                        logger.debug(file_data)
+                        self.write_file(file_data.filename, file_data.read())
+
+                return super().on_text_done(text)
+
+            def write_file(self, path, content):
+                full_path = working_dir.joinpath(path)
+                if full_path.exists():
+                    full_path.write_text(content)
+                return path
+
+            def on_tool_call_done(
+                self,
+                tool_call,
+            ) -> None:
+                if tool_call.type == "function":
+                    if tool_call.function.name == "write_file":
+                        logger.trace(tool_call.function.arguments)
+                        arguments = json.loads(tool_call.function.arguments)
+                        logger.trace(arguments)
+                        self.write_file(
+                            arguments["path"],
+                            arguments["content"],
+                        )
+                return super().on_tool_call_done(tool_call)
+
+        thread = self.client.beta.threads.create(
+            messages=[
+                {"role": "user", "content": prompt},
+            ],
+        )
+        with self.client.beta.threads.runs.stream(
+            thread_id=thread.id,
+            assistant_id=self.assistant.id,
+            event_handler=EventHandler(),
+        ) as stream:
+            stream.until_done()
+
+    def close(self):
+        self.client.beta.assistants.delete(self.assistant.id)
